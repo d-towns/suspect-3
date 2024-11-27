8585679bcfbff5aadda9f6b92b9cad1e48d0a777
dt0wns <dennis@dennistowns.com>
2024-11-27 01:36:04 -0500

    navbar style changes

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -15,19 +15,19 @@ const Navbar: React.FC = () => {
     <nav className="bg-gray-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
-            <div className="flex-shrink-0">
-            <Link to="/home" className="text-white font-bold hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm">
+        <Link to="/home" className="text-white text-2xl font-bold hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-left">
                 Suspect 3
             </Link>
+          <div className="flex items-center gap-10">
+            <div className="flex-shrink-0 ">
             </div>
-            <div className="ml-10 flex items-baseline space-x-4">
-              <Link to="/play" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+            <div className="flex items-baseline space-x-4">
+              <Link to="/play" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium">
                 Play
               </Link>
             </div>
-            <div className="ml-10 flex items-baseline space-x-4">
-              <Link to="/faq" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+            <div className=" flex items-baseline space-x-4">
+              <Link to="/faq" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium">
                 How To Play
               </Link>
             </div>
