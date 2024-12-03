e7a60f7f51f97da38aaea99d0c5f1aa810cdb371
dt0wns <dennis@dennistowns.com>
2024-12-03 11:06:08 -0500

    removing extra auth provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,7 @@ import {
 } from "react-router-dom";
 import "./index.css";
 
-import { AuthProvider } from "./context/auth.context";
-
 import router from "./routing/router";
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <AuthProvider>
     <RouterProvider router={router} />
-    </AuthProvider>
 );
