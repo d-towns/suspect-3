64f6e812ec250732c31beb9b2f2db85d14970870
dt0wns <dennis@dennistowns.com>
2024-12-03 11:08:12 -0500

    Home screen ui changes

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,36 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Box, Button, Card, Flex, Heading, Text, Inset } from '@radix-ui/themes';
 
 const Home: React.FC = () => {
     return (
-        <div className="relative flex flex-col items-center justify-center h-screen bg-white text-white">
-            <div className="absolute inset-0">
-                <img src="home-splash.webp" alt="splash" className="w-full h-full object-cover opacity-60" />
-            </div>
-            <div className="relative z-10">
-                <h1 className="text-4xl text-black text-center font-bold mb-8">Suspect 3</h1>
-                <div className="flex gap-4 items-center w-full">
-                    <Link
-                        to="/play"
-                        className="group relative w-48 flex justify-center py-4 px-6 border border-transparent text-2xl font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 hover:border-white hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-all"
-                    >
-                        Play
-                    </Link>
-                    <Link
-                        to="/faq"
-                        className="group relative w-48 flex justify-center py-4 px-6 border border-transparent text-2xl font-medium rounded-md text-white bg-green-600 hover:bg-green-700 hover:border-white hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transition-all"
-                    >
-                        How To Play
-                    </Link>
-                    <Link
-                        to="/report-bug"
-                        className="group relative w-48 flex justify-center py-4 px-6 border border-transparent text-2xl font-medium rounded-md text-white bg-red-600 hover:bg-red-700 hover:border-white hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-all"
-                    >
-                        Report a Bug
-                    </Link>
-                </div>
-            </div>
-        </div>
+        <Flex justify="center" align="center" style={{ height: '100vh', width: '100%' }}>
+            <Card size="3" variant="classic" style={{ width: '100%', maxWidth: '800px', padding: '50px'}} >
+                <Inset clip="padding-box" side="top" pb="current">
+                    <img
+                        src="home-splash.webp"
+                        alt="Suspect 3"
+                        style={{
+                            display: 'block',
+                            objectFit: 'cover',
+                            width: '100%',
+                            height: 400,
+                            backgroundColor: 'var(--gray-5)',
+                        }}
+                    />
+                    </Inset>
+                <Flex direction="column" gap="4" align="center" style={{maxWidth:'400px', margin:'auto'}}>
+                <Heading size="9" weight="bold" mb="4" align={'center'}>Suspect 3</Heading>
+                <Text size="4" mb={'3'} align={'center'}>A multiplayer game of deception and deduction</Text>
+                 <Link to="/play" style={{width: '100%'}}><Button size="2" style={{width: '100%'}}> Play</Button> </Link>
+                    <Link to="/faq" style={{width: '100%'}}><Button size="2" color='blue' style={{width: '100%'}}>How To Play</Button> </Link>
+                    <Link to="/leaderboard" style={{width: '100%'}}><Button size="2" color='orange' style={{width: '100%'}}>Leaderboard</Button> </Link>
+                </Flex>
+            </Card>
+        </Flex>
     );
 };
 
