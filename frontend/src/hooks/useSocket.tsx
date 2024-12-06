c8d8e118a63b9c8a6130888c3c645f9d8149ad4d
dt0wns <dennis@dennistowns.com>
2024-12-06 17:02:43 -0500

    fix: env urls

--- frontend/src/hooks/useSocket.tsx
+++ frontend/src/hooks/useSocket.tsx
@@ -5,7 +5,7 @@ import { useAuth } from '../context/auth.context';
 import { User, Invite } from '../models';
 import { useToast } from '../context/ToastContext/toast.context';
 
-const SOCKET_SERVER_URL = import.meta.env.VITE_SOCKET_URL;
+const SOCKET_SERVER_URL = import.meta.env.VITE_NODE_ENV === 'dev' ? import.meta.env.VITE_DEV_SOCKET_URL : import.meta.env.VITE_PROD_SOCKET_URL;
 const HEARTBEAT_INTERVAL = 5000; // 5 seconds
 
 export const useSocket = () => {
