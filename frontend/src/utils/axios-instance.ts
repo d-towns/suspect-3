ac63ec0ab66064ed3f88da01cd83ac5761f8f200
dt0wns <dennis@dennistowns.com>
2024-12-07 14:08:18 -0500

    fix: backend env vars

--- frontend/src/utils/axios-instance.ts
+++ frontend/src/utils/axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_NODE_ENV === 'dev' ? import.meta.env.VITE_BACKEND_URL : import.meta.env.VITE_PROD_BACKEND_URL,
+  baseURL: import.meta.env.VITE_NODE_ENV === 'dev' ? import.meta.env.VITE_DEV_BACKEND_URL : import.meta.env.VITE_PROD_BACKEND_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
