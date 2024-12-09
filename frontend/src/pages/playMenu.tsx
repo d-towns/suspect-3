48ce4b47c5f411e2b2258ba349eab766edcff300
dt0wns <dennis@dennistowns.com>
2024-12-09 01:14:54 -0500

    feat: hide single player mode

--- frontend/src/pages/playMenu.tsx
+++ frontend/src/pages/playMenu.tsx
@@ -18,13 +18,15 @@ interface ModeCardProps {
   imgSrc: string;
   altText: string;
   description?: string;
+  mode: 'single' | 'multi';
 }
 
 const ModeCard: React.FC<ModeCardProps> = ({
   createRoom,
   imgSrc,
   altText,
-  description
+  description,
+  mode
 }) => {
   return (
     <Box
@@ -32,11 +34,16 @@ const ModeCard: React.FC<ModeCardProps> = ({
       style={{
         position: 'relative',
         flex: 1,
-        cursor: 'pointer',
+        cursor: mode === 'single' ? 'default' : 'pointer',
         margin: '0 16px',
       }}
     >
-      <Card size="3" onClick={() => createRoom()} className='hover:scale-105 hover:border transition ease-in-out duration-200 '>
+      <Card 
+        size="3" 
+        onClick={() => mode === 'multi' && createRoom()} 
+        className={mode === 'multi' ? 'hover:scale-105 hover:border transition ease-in-out duration-200' : ''}
+        style={{ opacity: mode === 'single' ? 0.5 : 1 }}
+      >
         <Inset clip="padding-box" side="top" pb="current">
           <img
             src={imgSrc}
@@ -50,15 +57,31 @@ const ModeCard: React.FC<ModeCardProps> = ({
             }}
           />
         </Inset>
-        <Text as="p" align='center' size="8">
+        <Text as="p" align='center' size={{ lg: '7', md: '5', sm: '4'}} >
           <Strong>{altText} Mode</Strong>
         </Text>
         <Separator my="3" size="4" />
-        {description && <Text as="p" size={'4'}>{description}</Text>}
+        {description && <Text as="p" size={{ sm: '2', md: '3', lg: '4' }}>{description}</Text>}
         <Separator my="3" size="4" />
       </Card>
 
-          
+      {mode === 'single' && (
+        <Card
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: 'rgba(0, 0, 0, 0.8)',
+            padding: '20px',
+            borderRadius: '8px',
+          }}
+        >
+          <Text as="p" align='center' style={{ color: 'white' }} size="6">
+            <Strong>Coming Soon!</Strong>
+          </Text>
+        </Card>
+      )}
     </Box>
   );
 };
@@ -87,12 +110,14 @@ const PlayMenu: React.FC = () => {
           imgSrc="single-player-splash-2.webp"
           altText="Single Player"
           description="Enter the interrogation room as a detective! Interview the suspects in a real-time chat and use their testimonies along with the evidence to solve the case."
+          mode="single"
         />
         <ModeCard
           createRoom={createRoom}
           imgSrc="multi-player-splash.webp"
-          description='Play with friends in a private room. Share the invite code with your friends to join the room. The game will start when all players are ready.'
+          description='Clear your name as a suspect against the detective! The culprit is among you, but who? Invite your friends and use your wits to deceive the detective and avoid being framed...or found out.'
           altText="Multiplayer"
+          mode="multi"
         />
       </Flex>
     </Flex>
