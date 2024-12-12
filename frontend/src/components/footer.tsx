4521ca0f3435072a812de32185653e57a4336173
dt0wns <dennis@dennistowns.com>
2024-12-12 18:42:03 -0500

    fix: footer

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text } from '@radix-ui/themes';
 const Footer: React.FC = () => {
     return (
-        <footer className='text-center my-5'>
+        <footer className='text-center my-5 lg:fixed lg:bottom-0 lg:left-1/2 lg:transform lg:-translate-x-1/2'>
             <Text className='text-xs' as='p'>&copy; {new Date().getFullYear()} Towns Capital, LLC. All rights reserved.</Text>
         </footer>
     );
