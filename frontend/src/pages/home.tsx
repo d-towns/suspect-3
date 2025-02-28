64eac79517ba72cafc456f0b25941294d8200a91
dt0wns <dennis@dennistowns.com>
2025-02-27 23:04:54 -0500

    refactor: simplify home page layout and background animations

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -8,7 +8,7 @@ import { OffenseReportCard } from '../components/OffenseCard';
 import { OffenseReportItem } from '../models/game-state.model';
 import { useAuth } from '../context/auth.context';
 import LoginDialog from './login';
-import MiniOffenseCard from '../components/MiniOffenseCard';
+// import MiniOffenseCard from '../components/MiniOffenseCard';
 import StockTicker from '../components/StockTicker';
 
 //   location: string;
@@ -19,7 +19,7 @@ const sampleOffenses : OffenseReportItem[] = [
   {
     location: 'The Office',
     time: '9:00 AM',
-    description: 'The suspects in case #1E9Z were brought into HQ for questioning. None of them claimed to have seen the victim.',
+    description: 'The suspects in case #1E9Z were brought into HQ for questioning. None claimed to have seen the victim.',
     imgSrc: '/multi-player-splash.webp'
   },
   {
@@ -100,27 +100,14 @@ const Home: React.FC = () => {
     return (
       <>
     {/* Mobile Stock Ticker at the top */}
-    {isMobile && (
-      <div className="fixed top-[100px] left-0 w-full z-[-5] opacity-30">
-        <StockTicker direction="left" speed={30}>
-          {sampleOffenses.map((offense, index) => (
-            <MiniOffenseCard key={`top-${index}`} offenseReport={offense} />
-          ))}
-          {sampleOffenses.map((offense, index) => (
-            <MiniOffenseCard key={`top-repeat-${index}`} offenseReport={offense} />
-          ))}
-        </StockTicker>
-      </div>
-    )}
     
     <Flex
       direction="column"
       align="center"
       justify="center"
       px="4"
-      className='h-full rounded-lg p-9 m-auto bg-[#111110]'
+      className='h-full rounded-lg p-9 m-auto bg-[#111110] bg-opacity-50 backdrop-blur-sm'
     >
-      
       <AnimatedText className='text-7xl md:text-9xl main-header ' animationSpeed={200}  message="Suspect" />
       <AnimatedText className='text-sm main-link mb-9' animationSpeed={300} message="v0.93-alpha" />
       <AnimatedText className='text-sm md:text-lg text-center main-link mb-5' animationSpeed={50} message="Solve crimes through AI voice conversation" />
@@ -131,7 +118,7 @@ const Home: React.FC = () => {
 
       >
       <Flex direction="row" gap="4" my="6" className='h-full z-10'>
-        {user ? <Link to='/play' className='hover:scale-105 transition ease-in-out duration-200'> <Text size={{initial: '6', md: '8'}} as='span' className='main-link'>Play</Text></Link> :  <Text onClick={() => setLoginDialogOpen(true)} size={{initial: '6', md: '8'}} as='span' className='main-link'>Login</Text> }
+        {user ? <Link to='/play' className='hover:scale-105 transition ease-in-out duration-200'> <Text size={{initial: '6', md: '8'}} as='span' className='main-link z-10'>Play</Text></Link> :  <Text onClick={() => setLoginDialogOpen(true)} size={{initial: '6', md: '8'}} as='span' className='main-link'>Login</Text> }
        {!user && <LoginDialog open={loginDialogOpen} onOpenChange={() => setLoginDialogOpen(false)}/>}
         {/* <Link to='/login'> <Text size={'8'} as='span' className='main-link'>Play</Text></Link> */}
         <Separator orientation="vertical" size={'2'} />
@@ -141,9 +128,26 @@ const Home: React.FC = () => {
       </motion.div>
       </Flex>
 
-            {/* Mobile Stock Ticker at the bottom */}
-            {isMobile && (
-              <div className="fixed bottom-[200px] left-0 w-full z-[-5] opacity-30">
+
+            {/* Desktop random animated background - existing code */}
+              
+              <>
+      <div className="fixed bottom-[100px] left-0 w-full z-[-5] opacity-30">
+        <StockTicker direction="left" speed={30}>
+          {sampleOffenses.map((offense, index) => (
+            <div key={`top-${index}`} className='w-full mx-4 '>
+            <OffenseReportCard key={`top-${index}`} offenseReport={offense}  />
+            </div>
+          ))}
+          {sampleOffenses.map((offense, index) => (
+            <div key={`top-${index}`} className='w-full mx-4'>
+            <OffenseReportCard key={`top-${index}`} offenseReport={offense}  />
+            </div>
+          ))}
+        </StockTicker>
+      </div>
+
+      <div className="fixed top-[100px] left-0 w-full z-[-5] opacity-30">
                 <StockTicker direction="left" speed={25}>
                   {sampleAnimatedTexts.map((text, index) => (
                     <p 
@@ -165,71 +169,9 @@ const Home: React.FC = () => {
                   ))}
                 </StockTicker>
               </div>
-            )}
 
-            {/* Desktop random animated background - existing code */}
-            {!isMobile && (
-              <>
-                {sampleOffenses.map((offense, index) => (
-                  <div className='absolute h-fit top-0 left-0 z-[-10]' key={index}>
-                    <AnimatePresence>
-                      {cardsRendered[index].rendered &&
-                        <motion.div
-                          initial={{
-                            opacity: 0,
-                            ...(Math.random() > 0.5 ? { x: 100} : { y: Math.random() * 400 })
-                          }}
-                          
-                          animate={{ opacity: .20, x: 100, y: Math.random() * 400 }}
-                          exit={{
-                            opacity: 0,
-                            ...(Math.random() > 0.5 ? { x: -100 } : { y: -100 })
-                          }}
-                          transition={{ duration: 1, ease: 'easeInOut' }}
-                        >
-                          <OffenseReportCard key={index} offenseReport={offense}  />
-                        </motion.div>
-                      }
-                    </AnimatePresence>
-                  </div>
-                ))}
-                        <>
-                {sampleAnimatedTexts.map((text, i) => (
-                  <div className='absolute h-fit z-[-10]' key={i}>
-                  <AnimatePresence key={i}>
-                    {animatedTextsState[i].rendered && (
-                      <motion.div
-                        initial={{
-                          opacity: 0,
-                          ...(Math.random() > 0.5
-                            ? { x: Math.random() * 100 }
-                            : { y: Math.random() * 700 })
-                        }}
-                        animate={{
-                          opacity: 0.2,
-                          x: Math.random() * 400,
-                          y: Math.random() * 700
-                        }}
-                        exit={{
-                          opacity: 0,
-                          ...(Math.random() > 0.5 ? { x: -100 } : { y: -100 })
-                        }}
-                        transition={{ duration: 1, ease: 'easeInOut' }}
-                      >
-                        <AnimatedText
-                          className='text-5xl main-link '
-                          animationSpeed={200}
-                          message={text}
-                        />
-                      </motion.div>
-                    )}
-                  </AnimatePresence>
-                  </div>
-                ))}
-              </>
 
             </>
-            )}
       </>
 
     );
